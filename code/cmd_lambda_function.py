3a0137e305bc88a6d899394bc701524a4b03d5e9
Luis Ruelas <luise.ruelasz@gmail.com>
2023-06-29 10:07:48 -0600

    revert to main

--- code/cmd_lambda_function.py
+++ code/cmd_lambda_function.py
@@ -1,21 +1,5 @@
 from code.lambda_function import lambda_handler
 import sys
 
-if len(sys.argv) >= 5:
-    customerId = sys.argv[1]
-    customerKey = sys.argv[2]
-    brandId = sys.argv[3]
-    ids = sys.argv[4].split(",")
-    print(f"Customer ID: {customerId}")
-    print(f"Customer Key: {customerKey}")
-else:
-    print("Customer ID and Customer Key not provided.")
 
-EVENT = {
-    'customerId': customerId,
-    'customerKey': customerKey,
-    'brandId': brandId,
-    'ids': ids,
-}
-print('EVENT', EVENT)
-print(lambda_handler(EVENT, 2))
+print(lambda_handler(1, 2))
